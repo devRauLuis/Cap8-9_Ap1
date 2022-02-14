3fe8873cb87b03633699ae0e498dcecdd54c0f62
Raul Luis <soyrauluis@outlook.com>
2022-02-14 01:39:31 -0400

    Add Cap9 exercises

---
+++ Cap9/1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private List<Producto> productos = new List<Producto>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AgregarProductoClick(object sender, EventArgs e)
+        {
+            var nuevoProducto = new Producto() { Nombre = NombreTB.Text, Precio = Convert.ToDouble(PrecioTB.Text), Cantidad = Convert.ToInt32(CantidadTB.Text) };
+            Limpiar();
+            productos.Add(nuevoProducto);
+            ProductosDG.ItemsSource = productos;
+        }
+
+        private void Limpiar()
+        {
+            NombreTB.Text = "";
+            PrecioTB.Text = "";
+            CantidadTB.Text = "";
+        }
+    }
+
+
+    public struct Producto
+    {
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+        public int Cantidad { get; set; }
+
+    }
+}
---
+++ Cap9/3/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private List<Dueno> duenos = new List<Dueno>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AgregarClick(object sender, EventArgs e)
+        {
+            var dueno = new Dueno() { Nombre = DuenoNombreTB.Text, Direccion = DuenoDireccionTB.Text, Mascota = new Mascota() { Nombre = MascotaNombreTB.Text, Especie = MascotaEspecieTB.Text } };
+            Limpiar();
+            duenos.Add(dueno);
+            DuenosMascotasDG.ItemsSource = duenos;
+        }
+
+        private void Limpiar()
+        {
+            DuenoNombreTB.Text = "";
+            DuenoDireccionTB.Text = "";
+            MascotaNombreTB.Text = "";
+            MascotaEspecieTB.Text = "";
+        }
+    }
+
+    public struct Dueno
+    {
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public Mascota Mascota { get; set; }
+    }
+
+    public struct Mascota
+    {
+        public string Nombre { get; set; }
+        public string Especie { get; set; }
+    }
+
+}
